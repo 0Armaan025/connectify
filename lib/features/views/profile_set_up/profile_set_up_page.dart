d07fdd7cefc13bdeae22db13e9f4ed0fb275c275
Armaan <armaan33000@gmail.com>
2024-12-07 22:31:01 +0530

    TODO: fix profile set up thingy

---
+++ lib/features/views/profile_set_up/profile_set_up_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileSetUpPage extends StatefulWidget {
+  const ProfileSetUpPage({super.key});
+
+  @override
+  State<ProfileSetUpPage> createState() => _ProfileSetUpPageState();
+}
+
+class _ProfileSetUpPageState extends State<ProfileSetUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
