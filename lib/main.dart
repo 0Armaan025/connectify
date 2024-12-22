b12c29c2f81a782f08756345d977d72c14c6f81c
Armaan <armaan33000@gmail.com>
2024-12-22 19:58:23 +0530

    im bored

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:connectify/features/views/home/home_view.dart';
 import 'package:flutter/material.dart';
- 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
